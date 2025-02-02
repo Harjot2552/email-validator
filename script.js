26031caa6471a56b4e9827b85c6c0036a3d3061b
Harjot <hajotsingh2552@gmail.com>
2025-02-02 14:24:10 -0600

    Calling an API to validate the email

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 btn.addEventListener("click", async(e) => {
-   
+    e.preventDefault();
+    console.log("ih")
+    resultCont.innerHTML = `<img src="loading.svg" alt="">`
+    let key = "ema_live_EiQsAYWpDfuZcgqbICF4luRiqliQNLS3vNP1vZA6"
+    let email = document.getElementById("email").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result  = await res.json();
+    let str = ``;
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key] !== " "){
+            str = str + `<div>${key}: ${result[key]}</div>`
+
+        }
+        // console.log(key)
+
+    }
+
+    resultCont.innerHTML = str;
 })
